94d55c58e910cb9f38925738924cef7aff83c378
liubin <lbjobvip@163.com>
2016-07-22 15:12:42 +0800

    no message

--- LBLaunchImageAdDemo/LBLaunchImageAdDemo/AppDelegate.h
+++ LBLaunchImageAdDemo/LBLaunchImageAdDemo/AppDelegate.h
@@ -2,9 +2,10 @@
 //  AppDelegate.h
 //  LBLaunchImageAdDemo
 //
-//  Created by gold on 16/7/22.
+//  技术交流群：534926022（免费） 511040024(0.8/人付费)
+//  Created by gold on 16/6/12.
 //  Copyright © 2016年 Bison. All rights reserved.
-//
+//  iOS开发学习app下载https://itunes.apple.com/cn/app/it-blog-for-ios-developers/id1067787090?mt=8
 
 #import <UIKit/UIKit.h>
 
