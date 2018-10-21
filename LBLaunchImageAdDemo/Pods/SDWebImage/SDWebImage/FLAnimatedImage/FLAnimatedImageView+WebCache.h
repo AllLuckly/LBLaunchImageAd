40dda19a9616796e944e9cf2096600d5cfdac8c2
Biosn <435844159@qq.com>
2018-10-21 20:32:19 +0800

    去除sdwebimg的版本限制

--- LBLaunchImageAdDemo/Pods/SDWebImage/SDWebImage/FLAnimatedImage/FLAnimatedImageView+WebCache.h
+++ LBLaunchImageAdDemo/Pods/SDWebImage/SDWebImage/FLAnimatedImage/FLAnimatedImageView+WebCache.h
@@ -13,10 +13,23 @@
 #if __has_include(<FLAnimatedImage/FLAnimatedImage.h>)
 #import <FLAnimatedImage/FLAnimatedImage.h>
 #else
-#import "FLAnimatedImageView.h"
+#import "FLAnimatedImage.h"
 #endif
 
 #import "SDWebImageManager.h"
+
+/**
+ *  FLAnimatedImage is not a subclass of UIImage, so it's not possible to store it into the memory cache currently. However, for performance issue and cell reuse on FLAnimatedImageView, we use associate object to bind a FLAnimatedImage into UIImage when an animated GIF image load. For most cases, you don't need to touch this.
+ */
+@interface UIImage (FLAnimatedImage)
+
+/**
+ *  The FLAnimatedImage associated to the UIImage instance when an animated GIF image load.
+ *  For most cases this is read-only and you should avoid manually setting this value. Util some cases like using placeholder with a `FLAnimatedImage`.
+ */
+@property (nonatomic, strong, nullable) FLAnimatedImage *sd_FLAnimatedImage;
+
+@end
 
 
 /**
@@ -26,12 +39,33 @@
 @interface FLAnimatedImageView (WebCache)
 
 /**
+ * Optimal frame cache size of FLAnimatedImage during initializer. (1.0.11 version later)
+ * This value will help you set `optimalFrameCacheSize` arg of FLAnimatedImage initializer after image load.
+ * Defaults to 0.
+ */
+@property (nonatomic, assign) NSUInteger sd_optimalFrameCacheSize;
+
+/**
+ * Predrawing control of FLAnimatedImage during initializer. (1.0.11 version later)
+ * This value will help you set `predrawingEnabled` arg of FLAnimatedImage initializer after image load.
+ * Defaults to YES.
+ */
+@property (nonatomic, assign) BOOL sd_predrawingEnabled;
+
+/**
+ * Cache control for associated FLAnimatedImage object for memory cache. When enabled, we will bind created FLAnimatedImage instance to UIImage, and store it into memory cache to avoid create this instance cause decoding performance. See `UIImage+FLAnimatedImage`.
+ * When enabled, this may consume more memory, if you are facing memory issue, disable it and let FLAnimatedImage been created just in time and dealloced as it not been used. However, when disabled, this may impact performance since we need query disk cache, create FLAnimatedImage and decoding even when the current GIF url is cached.
+ * Defatuls to YES;
+ */
+@property (nonatomic, assign) BOOL sd_cacheFLAnimatedImage;
+
+/**
  * Load the image at the given url (either from cache or download) and load it in this imageView. It works with both static and dynamic images
  * The download is asynchronous and cached.
  *
  * @param url The url for the image.
  */
-- (void)sd_setImageWithURL:(nullable NSURL *)url;
+- (void)sd_setImageWithURL:(nullable NSURL *)url NS_REFINED_FOR_SWIFT;
 
 /**
  * Load the image at the given url (either from cache or download) and load it in this imageView. It works with both static and dynamic images
@@ -42,7 +76,7 @@
  * @param placeholder The image to be set initially, until the image request finishes.
  */
 - (void)sd_setImageWithURL:(nullable NSURL *)url
-          placeholderImage:(nullable UIImage *)placeholder;
+          placeholderImage:(nullable UIImage *)placeholder NS_REFINED_FOR_SWIFT;
 
 /**
  * Load the image at the given url (either from cache or download) and load it in this imageView. It works with both static and dynamic images
@@ -55,7 +89,7 @@
  */
 - (void)sd_setImageWithURL:(nullable NSURL *)url
           placeholderImage:(nullable UIImage *)placeholder
-                   options:(SDWebImageOptions)options;
+                   options:(SDWebImageOptions)options NS_REFINED_FOR_SWIFT;
 
 /**
  * Load the image at the given url (either from cache or download) and load it in this imageView. It works with both static and dynamic images
@@ -86,7 +120,7 @@
  */
 - (void)sd_setImageWithURL:(nullable NSURL *)url
           placeholderImage:(nullable UIImage *)placeholder
-                 completed:(nullable SDExternalCompletionBlock)completedBlock;
+                 completed:(nullable SDExternalCompletionBlock)completedBlock NS_REFINED_FOR_SWIFT;
 
 /**
  * Load the image at the given url (either from cache or download) and load it in this imageView. It works with both static and dynamic images
--- LBLaunchImageAdDemo/Pods/SDWebImage/SDWebImage/FLAnimatedImage/FLAnimatedImageView+WebCache.m
+++ LBLaunchImageAdDemo/Pods/SDWebImage/SDWebImage/FLAnimatedImage/FLAnimatedImageView+WebCache.m
@@ -13,10 +13,76 @@
 #import "UIView+WebCacheOperation.h"
 #import "UIView+WebCache.h"
 #import "NSData+ImageContentType.h"
-#import "FLAnimatedImage.h"
 #import "UIImageView+WebCache.h"
+#import "UIImage+MultiFormat.h"
+
+static inline FLAnimatedImage * SDWebImageCreateFLAnimatedImage(FLAnimatedImageView *imageView, NSData *imageData) {
+    if ([NSData sd_imageFormatForImageData:imageData] != SDImageFormatGIF) {
+        return nil;
+    }
+    FLAnimatedImage *animatedImage;
+    // Compatibility in 4.x for lower version FLAnimatedImage.
+    if ([FLAnimatedImage respondsToSelector:@selector(initWithAnimatedGIFData:optimalFrameCacheSize:predrawingEnabled:)]) {
+        animatedImage = [[FLAnimatedImage alloc] initWithAnimatedGIFData:imageData optimalFrameCacheSize:imageView.sd_optimalFrameCacheSize predrawingEnabled:imageView.sd_predrawingEnabled];
+    } else {
+        animatedImage = [[FLAnimatedImage alloc] initWithAnimatedGIFData:imageData];
+    }
+    return animatedImage;
+}
+
+@implementation UIImage (FLAnimatedImage)
+
+- (FLAnimatedImage *)sd_FLAnimatedImage {
+    return objc_getAssociatedObject(self, @selector(sd_FLAnimatedImage));
+}
+
+- (void)setSd_FLAnimatedImage:(FLAnimatedImage *)sd_FLAnimatedImage {
+    objc_setAssociatedObject(self, @selector(sd_FLAnimatedImage), sd_FLAnimatedImage, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+@end
 
 @implementation FLAnimatedImageView (WebCache)
+
+// These property based options will moved to `SDWebImageContext` in 5.x, to allow per-image-request level options instead of per-imageView-level options
+- (NSUInteger)sd_optimalFrameCacheSize {
+    NSUInteger optimalFrameCacheSize = 0;
+    NSNumber *value = objc_getAssociatedObject(self, @selector(sd_optimalFrameCacheSize));
+    if ([value isKindOfClass:[NSNumber class]]) {
+        optimalFrameCacheSize = value.unsignedShortValue;
+    }
+    return optimalFrameCacheSize;
+}
+
+- (void)setSd_optimalFrameCacheSize:(NSUInteger)sd_optimalFrameCacheSize {
+    objc_setAssociatedObject(self, @selector(sd_optimalFrameCacheSize), @(sd_optimalFrameCacheSize), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (BOOL)sd_predrawingEnabled {
+    BOOL predrawingEnabled = YES;
+    NSNumber *value = objc_getAssociatedObject(self, @selector(sd_predrawingEnabled));
+    if ([value isKindOfClass:[NSNumber class]]) {
+        predrawingEnabled = value.boolValue;
+    }
+    return predrawingEnabled;
+}
+
+- (void)setSd_predrawingEnabled:(BOOL)sd_predrawingEnabled {
+    objc_setAssociatedObject(self, @selector(sd_predrawingEnabled), @(sd_predrawingEnabled), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (BOOL)sd_cacheFLAnimatedImage {
+    BOOL cacheFLAnimatedImage = YES;
+    NSNumber *value = objc_getAssociatedObject(self, @selector(sd_cacheFLAnimatedImage));
+    if ([value isKindOfClass:[NSNumber class]]) {
+        cacheFLAnimatedImage = value.boolValue;
+    }
+    return cacheFLAnimatedImage;
+}
+
+- (void)setSd_cacheFLAnimatedImage:(BOOL)sd_cacheFLAnimatedImage {
+    objc_setAssociatedObject(self, @selector(sd_cacheFLAnimatedImage), @(sd_cacheFLAnimatedImage), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
 
 - (void)sd_setImageWithURL:(nullable NSURL *)url {
     [self sd_setImageWithURL:url placeholderImage:nil options:0 progress:nil completed:nil];
@@ -53,13 +119,42 @@
                              options:options
                         operationKey:nil
                        setImageBlock:^(UIImage *image, NSData *imageData) {
-                           SDImageFormat imageFormat = [NSData sd_imageFormatForImageData:imageData];
-                           if (imageFormat == SDImageFormatGIF) {
-                               weakSelf.animatedImage = [FLAnimatedImage animatedImageWithGIFData:imageData];
-                               weakSelf.image = nil;
+                           __strong typeof(weakSelf)strongSelf = weakSelf;
+                           if (!strongSelf) {
+                               return;
+                           }
+                           // Step 1. Check memory cache (associate object)
+                           FLAnimatedImage *associatedAnimatedImage = image.sd_FLAnimatedImage;
+                           if (associatedAnimatedImage) {
+                               // Asscociated animated image exist
+                               strongSelf.animatedImage = associatedAnimatedImage;
+                               strongSelf.image = nil;
+                               return;
+                           }
+                           // Step 2. Check if original compressed image data is "GIF"
+                           BOOL isGIF = (image.sd_imageFormat == SDImageFormatGIF || [NSData sd_imageFormatForImageData:imageData] == SDImageFormatGIF);
+                           if (!isGIF) {
+                               strongSelf.image = image;
+                               strongSelf.animatedImage = nil;
+                               return;
+                           }
+                           // Step 3. Check if data exist or query disk cache
+                           if (!imageData) {
+                               NSString *key = [[SDWebImageManager sharedManager] cacheKeyForURL:url];
+                               imageData = [[SDImageCache sharedImageCache] diskImageDataForKey:key];
+                           }
+                           // Step 4. Create FLAnimatedImage
+                           FLAnimatedImage *animatedImage = SDWebImageCreateFLAnimatedImage(strongSelf, imageData);
+                           // Step 5. Set animatedImage or normal image
+                           if (animatedImage) {
+                               if (strongSelf.sd_cacheFLAnimatedImage) {
+                                   image.sd_FLAnimatedImage = animatedImage;
+                               }
+                               strongSelf.animatedImage = animatedImage;
+                               strongSelf.image = nil;
                            } else {
-                               weakSelf.image = image;
-                               weakSelf.animatedImage = nil;
+                               strongSelf.animatedImage = nil;
+                               strongSelf.image = image;
                            }
                        }
                             progress:progressBlock
